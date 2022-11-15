ea73409b496565455d732ae54bc98b0816efd0d1
NoamMishaeli <noamico123@gmail.com>
2022-11-16 00:40:48 +0200

    Added onPause and onResume with gameLogic

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -19,6 +22,7 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Constants
     static final int NUMBER_OF_ROWS = 5;
     static final int NUMBER_OF_COLUMNS = 3;
     static final int DELAY_FIRST_BLOCK = 300;
@@ -28,17 +32,27 @@ public class MainActivity extends AppCompatActivity {
     static final int CENTER_LANE = 13;
     static final int RIGHT_LANE = 14;
 
+    // General
     protected boolean shouldMoveForward = true;
-    protected int carLocation;
-    protected ArrayList<LinearLayout> blocksArray;
 
+    //Generating block's locations, array for blocks and Timers for each block
     protected int firstBlockLocation = new Random().nextInt(NUMBER_OF_COLUMNS);
     protected int secondBlockLocation = new Random().nextInt(NUMBER_OF_COLUMNS);
+    protected ArrayList<LinearLayout> blocksArray;
+    Timer secondBlockTimer;
+    Timer firstBlockTimer;
 
-    //Define car's linear layouts
+    //Define car's linear layouts and initial location
     protected LinearLayout right_car;
     protected LinearLayout center_car;
     protected LinearLayout left_car;
+    protected int carLocation = 13;
+
+    //Define heart's ImageView and initial total number of hearts
+    protected ImageView right_heart;
+    protected ImageView center_heart;
+    protected ImageView left_heart;
+    protected int numberOfHearts = 3;
 
     //Define Buttons
     protected ImageButton right_button;
@@ -82,10 +96,132 @@ public class MainActivity extends AppCompatActivity {
         start_btn.setOnClickListener(v -> startGame());
     }
 
-    Timer secondBlockTimer = new Timer();
-    Timer firstBlockTimer = new Timer();
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopGame();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        start_btn.setVisibility(View.VISIBLE);
+    }
+
+    /*
+     * ##############################
+     * Init App
+     * ##############################
+     * */
+    private void initCarLocation() {
+        Log.i("INIT","car in the center");
+        center_car.setVisibility(View.VISIBLE);
+        right_car.setVisibility(View.INVISIBLE);
+        left_car.setVisibility(View.INVISIBLE);
+    }
+
+    private void initParameters() {
+        Log.i("INIT","start init params");
+        initBlocksLinearLayoutViews();
+        initCarsLinearLayoutViews();
+        initHeartsImageView();
+        initButtons();
+    }
+
+    private void initCarsLinearLayoutViews() {
+        Log.i("INIT","car's possible locations");
+        left_car = (LinearLayout) findViewById(R.id.sixth_row_first_column);
+        center_car = (LinearLayout) findViewById(R.id.sixth_row_second_column);
+        right_car = (LinearLayout) findViewById(R.id.sixth_row_third_column);
+    }
+
+    private void initHeartsImageView() {
+        Log.i("INIT","hearts");
+        left_heart = (ImageView) findViewById(R.id.top_bar_left_side_first_heart);
+        center_heart = (ImageView) findViewById(R.id.top_bar_left_side_second_heart);
+        right_heart = (ImageView) findViewById(R.id.top_bar_left_side_third_heart);
+    }
+
+    private void initButtons() {
+        Log.i("INIT","buttons");
+        right_button = (ImageButton) findViewById(R.id.right_button);
+        left_button = (ImageButton) findViewById(R.id.left_button);
+        start_btn = (Button) findViewById(R.id.start_btn);
+    }
+
+    private void initBlocksLinearLayoutViews() {
+        Log.i("INIT","blocks locations");
+        first_first = (LinearLayout) findViewById(R.id.first_row_first_column);
+        first_second = (LinearLayout) findViewById(R.id.first_row_second_column);
+        first_third = (LinearLayout) findViewById(R.id.first_row_third_column);
+
+        second_first = (LinearLayout) findViewById(R.id.second_row_first_column);
+        second_second = (LinearLayout) findViewById(R.id.second_row_second_column);
+        second_third = (LinearLayout) findViewById(R.id.second_row_third_column);
+
+        third_first = (LinearLayout) findViewById(R.id.third_row_first_column);
+        third_second = (LinearLayout) findViewById(R.id.third_row_second_column);
+        third_third = (LinearLayout) findViewById(R.id.third_row_third_column);
+
+        fourth_first = (LinearLayout) findViewById(R.id.fourth_row_first_column);
+        fourth_second = (LinearLayout) findViewById(R.id.fourth_row_second_column);
+        fourth_third = (LinearLayout) findViewById(R.id.fourth_row_third_column);
+
+        fifth_first = (LinearLayout) findViewById(R.id.fifth_row_first_column);
+        fifth_second = (LinearLayout) findViewById(R.id.fifth_row_second_column);
+        fifth_third = (LinearLayout) findViewById(R.id.fifth_row_third_column);
+    }
+
+    private void initBlocksState() {
+        Log.i("STATE","all blocks to invisible state except for two block");
+        for (int i = 0; i < blocksArray.size(); i++){
+            blocksArray.get(i).setVisibility(View.INVISIBLE);
+        }
+
+        while (firstBlockLocation == secondBlockLocation){
+            secondBlockLocation = new Random().nextInt(NUMBER_OF_COLUMNS);
+        }
+
+        blocksArray.get(firstBlockLocation).setVisibility(View.VISIBLE);
+        blocksArray.get(secondBlockLocation).setVisibility(View.VISIBLE);
+    }
+
+    private ArrayList<LinearLayout> getBlocksArray() {
+        Log.i("INIT","blocks array list");
+        ArrayList<LinearLayout> blocksArray = new ArrayList<>();
+
+        blocksArray.add(first_first);
+        blocksArray.add(first_second);
+        blocksArray.add(first_third);
+
+        blocksArray.add(second_first);
+        blocksArray.add(second_second);
+        blocksArray.add(second_third);
+
+        blocksArray.add(third_first);
+        blocksArray.add(third_second);
+        blocksArray.add(third_third);
+
+        blocksArray.add(fourth_first);
+        blocksArray.add(fourth_second);
+        blocksArray.add(fourth_third);
+
+        blocksArray.add(fifth_first);
+        blocksArray.add(fifth_second);
+        blocksArray.add(fifth_third);
+
+        Log.i("length of blocks array", String.valueOf(blocksArray.size()));
+
+        return blocksArray;
+    }
+
+    /*
+    * ##############################
+    * Timers
+    * ##############################
+    * */
     private void startFirstBlockTimer() {
+        firstBlockTimer = new Timer();
         firstBlockTimer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
@@ -101,6 +237,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startSecondBlockTimer() {
+        secondBlockTimer = new Timer();
         secondBlockTimer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
@@ -115,19 +252,26 @@ public class MainActivity extends AppCompatActivity {
         }, DELAY_SECOND_BLOCK, DELAY_SECOND_BLOCK);
     }
 
-    private void stopTimer() {
+    private void stopGame() {
         firstBlockTimer.cancel();
         secondBlockTimer.cancel();
     }
 
-    private void updateFirstBlockUI() {
-        moveFirstBlock();
+    private void startGame() {
+        left_heart.setVisibility(View.VISIBLE);
+        center_heart.setVisibility(View.VISIBLE);
+        right_heart.setVisibility(View.VISIBLE);
+        start_btn.setVisibility(View.INVISIBLE);
+
+        startFirstBlockTimer();
+        startSecondBlockTimer();
     }
 
-    private void updateSecondBlockUI() {
-        moveSecondBlock();
-    }
-
+    /*
+     * ##############################
+     * Game Control and UI Handlers
+     * ##############################
+     * */
     private void moveFirstBlock() {
         shouldMoveForward = true;
 
@@ -175,61 +319,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void isGameOver() {
         if (carLocation == secondBlockLocation || carLocation == firstBlockLocation ){
-            stopTimer();
+            numberOfHearts -= 1;
+            handleHeartsAndFinishGame();
             Log.i("BBOOMMM", "carLocation: " + carLocation + "Lane: " + firstBlockLocation);
             Log.i("BBOOMMM", "carLocation: " + carLocation + "Lane: " + secondBlockLocation);
         }
-    }
-
-    private void initCarLocation() {
-        Log.i("INIT","car in the center");
-        center_car.setVisibility(View.VISIBLE);
-        right_car.setVisibility(View.INVISIBLE);
-        left_car.setVisibility(View.INVISIBLE);
-    }
-
-    private void initParameters() {
-        Log.i("INIT","start init params");
-        initBlocksLinearLayoutViews();
-        initCarsLinearLayoutViews();
-        initButtons();
-    }
-
-    private void initCarsLinearLayoutViews() {
-        Log.i("INIT","car's possible locations");
-        left_car = (LinearLayout) findViewById(R.id.sixth_row_first_column);
-        center_car = (LinearLayout) findViewById(R.id.sixth_row_second_column);
-        right_car = (LinearLayout) findViewById(R.id.sixth_row_third_column);
-    }
-
-    private void initButtons() {
-        Log.i("INIT","buttons");
-        right_button = (ImageButton) findViewById(R.id.right_button);
-        left_button = (ImageButton) findViewById(R.id.left_button);
-        start_btn = (Button) findViewById(R.id.start_btn);
-    }
-
-    private void initBlocksLinearLayoutViews() {
-        Log.i("INIT","blocks locations");
-        first_first = (LinearLayout) findViewById(R.id.first_row_first_column);
-        first_second = (LinearLayout) findViewById(R.id.first_row_second_column);
-        first_third = (LinearLayout) findViewById(R.id.first_row_third_column);
-
-        second_first = (LinearLayout) findViewById(R.id.second_row_first_column);
-        second_second = (LinearLayout) findViewById(R.id.second_row_second_column);
-        second_third = (LinearLayout) findViewById(R.id.second_row_third_column);
-
-        third_first = (LinearLayout) findViewById(R.id.third_row_first_column);
-        third_second = (LinearLayout) findViewById(R.id.third_row_second_column);
-        third_third = (LinearLayout) findViewById(R.id.third_row_third_column);
-
-        fourth_first = (LinearLayout) findViewById(R.id.fourth_row_first_column);
-        fourth_second = (LinearLayout) findViewById(R.id.fourth_row_second_column);
-        fourth_third = (LinearLayout) findViewById(R.id.fourth_row_third_column);
-
-        fifth_first = (LinearLayout) findViewById(R.id.fifth_row_first_column);
-        fifth_second = (LinearLayout) findViewById(R.id.fifth_row_second_column);
-        fifth_third = (LinearLayout) findViewById(R.id.fifth_row_third_column);
     }
 
     private void moveCarToRight() {
@@ -264,53 +358,43 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void startGame() {
-        start_btn.setVisibility(View.INVISIBLE);
-        startFirstBlockTimer();
-        startSecondBlockTimer();
+    private void updateFirstBlockUI() {
+        moveFirstBlock();
     }
 
-    private void initBlocksState() {
-        Log.i("STATE","all blocks to invisible state except for two block");
-        for (int i = 0; i < blocksArray.size(); i++){
-            blocksArray.get(i).setVisibility(View.INVISIBLE);
+    private void updateSecondBlockUI() {
+        moveSecondBlock();
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void handleHeartsAndFinishGame() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(500);
         }
 
-        while (firstBlockLocation == secondBlockLocation){
-            secondBlockLocation = new Random().nextInt(NUMBER_OF_COLUMNS);
+        if (numberOfHearts == 2) left_heart.setVisibility(View.INVISIBLE);
+
+        if (numberOfHearts == 1) center_heart.setVisibility(View.INVISIBLE);
+
+        if (numberOfHearts <= 0) {
+            right_heart.setVisibility(View.INVISIBLE);
+            stopGame();
+            start_btn.setText("Try Again?");
+            start_btn.setVisibility(View.VISIBLE);
+            numberOfHearts = 3;
+
+            // Vibrate for 500 milliseconds
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+            } else {
+                //deprecated in API 26
+                v.vibrate(1000);
+            }
         }
-
-        blocksArray.get(firstBlockLocation).setVisibility(View.VISIBLE);
-        blocksArray.get(secondBlockLocation).setVisibility(View.VISIBLE);
     }
-
-    private ArrayList<LinearLayout> getBlocksArray() {
-        Log.i("INIT","blocks array list");
-        ArrayList<LinearLayout> blocksArray = new ArrayList<>();
-
-        blocksArray.add(first_first);
-        blocksArray.add(first_second);
-        blocksArray.add(first_third);
-
-        blocksArray.add(second_first);
-        blocksArray.add(second_second);
-        blocksArray.add(second_third);
-
-        blocksArray.add(third_first);
-        blocksArray.add(third_second);
-        blocksArray.add(third_third);
-
-        blocksArray.add(fourth_first);
-        blocksArray.add(fourth_second);
-        blocksArray.add(fourth_third);
-
-        blocksArray.add(fifth_first);
-        blocksArray.add(fifth_second);
-        blocksArray.add(fifth_third);
-
-        Log.i("length of blocks array", String.valueOf(blocksArray.size()));
-
-        return blocksArray;
-    }
-
 }
